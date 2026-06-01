1092c545ee99c0bce0abd637bc7e59aa93bfe7e7
yaosquared <marioyaoyao14@gmail.com>
2026-06-01 11:14:29 +0800

    fix: added missing url on auth apis

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -3,11 +3,11 @@ import axios from 'axios'
 import type { TLoginSchema, TRegisterSchema } from '@/types/auth'
 
 export const login = async (payload: TLoginSchema) => {
-  const res = await axios.post(`/api/auth/login`, payload)
+  const res = await axios.post(`${import.meta.env.VITE_API_URL}/api/auth/login`, payload)
   return res.data
 }
 
 export const register = async (payload: TRegisterSchema) => {
-  const res = await axios.post(`/api/auth/register`, payload)
+  const res = await axios.post(`${import.meta.env.VITE_API_URL}/api/auth/register`, payload)
   return res.data
 }
