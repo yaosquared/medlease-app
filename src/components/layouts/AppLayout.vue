804811381ed54eae3eaa6d3136f854345b516e1d
yaosquared <marioyaoyao14@gmail.com>
2026-06-17 14:40:34 +0800

    fix: fix: close sidebar on footer click in mobile view

--- src/components/layouts/AppLayout.vue
+++ src/components/layouts/AppLayout.vue
@@ -77,6 +77,7 @@ const userItems = computed<DropdownMenuItem[][]>(() => [
       icon: 'i-lucide-user',
       onSelect() {
         router.push('/profile')
+        if (isMobile.value) open.value = false
       },
     },
   ],
@@ -92,6 +93,7 @@ const userItems = computed<DropdownMenuItem[][]>(() => [
           checked: colorMode.value === 'light',
           onUpdateChecked(checked: boolean) {
             if (checked) colorMode.value = 'light'
+            if (isMobile.value) open.value = false
           },
           onSelect(e: Event) {
             e.preventDefault()
@@ -104,6 +106,7 @@ const userItems = computed<DropdownMenuItem[][]>(() => [
           checked: colorMode.value === 'dark',
           onUpdateChecked(checked: boolean) {
             if (checked) colorMode.value = 'dark'
+            if (isMobile.value) open.value = false
           },
           onSelect(e: Event) {
             e.preventDefault()
